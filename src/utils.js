fa028170716aefac3cb66c36943089e4198441ca
AlisaKravchenko <AlisaKravchenko@users.noreply.github.com>
2025-04-03 14:17:03 +0300

    fixed getRepeatTimeEnding

--- src/utils.js
+++ src/utils.js
@@ -88,30 +88,28 @@ export function getRepeatTime(repeatsCount, state){
 }
 
 export function getRepeatTimeEnding(state){
-    let timeLearning = 0
+    let timeLearning = 0;
+    let interval
     for (let key in state.repeat) {
-        if (state.repeat[key].length) {
-            let flag = 0
-            if (state.repeat[key][0] && flag === 0){
-                flag = 1
-                const interval = key - Date.now() + state.repeat[key][0].time
-                if (+key >= 604800000){
-                    timeLearning = Math.round(interval / 604800000) + ' нед.'
-                } else if (+key >= 86400000){
-                    timeLearning = Math.round(interval / 86400000) + ' дн.'
-                } else if (+key >= 7200000) {
-                    timeLearning = Math.round(interval / 3600000) + ' ч.'
-                } else{
-                    timeLearning = Math.round(interval / 60000) + ' мин.'
-                }
-            }
+        for (let key1 in state.repeat[key]) {
+           
+            interval = Math.min(interval,key - Date.now() + key1.time)
                 
             
         }
     }
+    
+    if (interval >= 604800000){
+        timeLearning = Math.round(interval / 604800000) + ' нед.'
+    } else if (interval >= 86400000){
+        timeLearning = Math.round(interval / 86400000) + ' дн.'
+    } else if (interval >= 7200000) {
+        timeLearning = Math.round(interval / 3600000) + ' ч.'
+    } else{
+        timeLearning = Math.round(interval / 60000) + ' мин.'
+    }
     return timeLearning
 }
-
 
 export function translateInput(translateWord, attempts, firstShowLang, voiceEnWord) {
     const input = document.querySelector('[data-type="input-translate"]')
