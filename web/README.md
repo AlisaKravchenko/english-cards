63099f92f5961230c2cae4722d9e3381ec7659f0
Alice Kravchenko <75129763+AlisaKravchenko@users.noreply.github.com>
2025-12-26 17:34:23 +0300

    Delete web directory

--- web/README.md
+++
@@ -1,4 +0,0 @@
-# Getting Started with English Cards
-
-This project was bootstrapped with [demo](https://english-cards.vercel.app).
-
--- web/src/constants.js
+++
@@ -1,3 +0,0 @@
-export const BASE_BACKGROUND = '#00897b'
-export const LIGHT_THEME_BACKGROUND = '#edeef0'
-export const DARK_THEME_BACKGROUND = '#212425'
